fe4b16c1960c3e76e6e22f97fe1fc575dd131b51
leikan <kanlei@live.com>
2019-04-04 16:20:02 +0800

    add generate mnemonic

--- AdaSharp/AdaUtils.cs
+++ AdaSharp/AdaUtils.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace AdaSharp
@@ -34,6 +35,21 @@ namespace AdaSharp
                     return amount;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(toUnits));
+            }
+        }
+        
+        /// 1. Pick a long sentence using a wide variety of characters (uppercase, lowercase, whitespace, punctuation, etc).
+        ///    Using a computer to randomly generate a passphrase is best, as humans aren't a good source of randomness.
+        /// 2. Compute an appropriate hash of this passphrase. 
+        ///    You'll need to use an algorithm that yields a 32-byte long string (e.g. SHA256 or BLAKE2b).
+        /// 3. Hex-encode the 32-byte hash into a 64-byte sequence of bytes.
+        public static string GeneratePassphrase(string passphrase)
+        {
+            var bytes = Encoding.UTF8.GetBytes(passphrase);
+            using (var sha256 = SHA256.Create())
+            {
+                var hashBytes = sha256.ComputeHash(bytes);
+                return BitConverter.ToString(hashBytes).Replace("-", "");
             }
         }
     }
--- AdaSharpTest/Program.cs
+++ AdaSharpTest/Program.cs
@@ -11,6 +11,8 @@ namespace AdaSharpTest
         static void Main(string[] args)
         {
 
+            Console.WriteLine(Mnemonic.GenerateMnemonic());
+
             var service = new AdaWalletService("https://172.18.1.219:8090", "", "", false);
             var tx = service.GetTransactionsByFilterAsync("2cWKMJemoBak8MPHidBtVJG63rD76dD21vTk4dLzxoajfZSEgrLRuPTXuwe8M8vvdJCnG", 2147483648, id: "0f7baf6ba6827adf37b0d0be2c408b9745f35670c415af906fd74a560d65436b", idOperator: AdaSharp.Wallet.DTOs.Request.FilterOperator.EQ).Result;
 
